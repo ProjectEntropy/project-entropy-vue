a13d21c4eb7c817f0379358d5a7b2208c08c13b8
Joran <joran.k@gmail.com>
2018-11-20 20:24:54 +1300

    package bump

--- src/components/Missions.vue
+++ src/components/Missions.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="">
-    Missions
+  <div class="container-fluid">
+    <div class="row">  
+      <div class="col-sm-12">
+        <h1>Missions</h1>
+      </div>
+
+      <div class="card">
+        <img class="card-img-top" src="./../assets/mauihook.png" alt="Mauihook">
+        <div class="card-body">
+          <h5 class="card-title">PureWaves Plastic Recycling</h5>
+          <p class="card-text">Maui's Hook</p>
+          <p class="card-text"><small class="text-muted">16th May 2018</small></p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
